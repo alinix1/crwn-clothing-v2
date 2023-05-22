aab1f9460390c757beb12d608c260b5571bbd971
Ali <anix1@kent.edu>
2023-05-22 17:09:18 -0600

    add Redux toolkit for state management

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,37 +1,90 @@
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
-import { CheckoutItemContainer } from './checkout-item.styles';
+import { useDispatch } from 'react-redux';
+
+import {
+  clearItemFromCart,
+  addItemToCart,
+  removeItemFromCart,
+} from '../../store/cart/cart.reducer';
+
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  BaseSpan,
+  Quantity,
+  Arrow,
+  Value,
+  RemoveButton,
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
-  const { clearItemFromCart, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
+  const dispatch = useDispatch();
 
-  const clearItemHandler = () => clearItemFromCart(cartItem);
-  const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => removeItemToCart(cartItem);
+  const clearItemHandler = () => dispatch(clearItemFromCart(cartItem));
+  const addItemHandler = () => dispatch(addItemToCart(cartItem));
+  const removeItemHandler = () => dispatch(removeItemFromCart(cartItem));
+
   return (
     <CheckoutItemContainer>
-      <div className="image-container">
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity"> </span>
-      <div className="arrow" onClick={removeItemHandler}>
-        &#10094;
-      </div>
-
-      <span className="value">{quantity}</span>
-      <div className="arrow" onClick={addItemHandler}>
-        &#10095;
-      </div>
-
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={clearItemHandler}>
-        &#10005;
-      </div>
+      </ImageContainer>
+      <BaseSpan> {name} </BaseSpan>
+      <Quantity>
+        <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+      </Quantity>
+      <BaseSpan> {price}</BaseSpan>
+      <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
     </CheckoutItemContainer>
   );
 };
 
 export default CheckoutItem;
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectCartItems } from '../../store/cart/cart.selector';
+// import {
+//   addItemToCart,
+//   clearItemFromCart,
+//   removeItemFromCart,
+// } from '../../store/cart/cart.reducer';
+// import { CheckoutItemContainer } from './checkout-item.styles';
+
+// const CheckoutItem = ({ cartItem }) => {
+//   const { name, imageUrl, price, quantity } = cartItem;
+//   console.log(imageUrl);
+//   const cartItems = useSelector(selectCartItems);
+//   const dispatch = useDispatch();
+
+//   const clearItemHandler = () =>
+//     dispatch(clearItemFromCart(cartItems, cartItem));
+//   const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+//   const removeItemHandler = () =>
+//     dispatch(removeItemFromCart(cartItems, cartItem));
+//   return (
+//     <CheckoutItemContainer>
+//       <div className="image-container">
+//         <img src={imageUrl} alt={`${name}`} />
+//       </div>
+//       <span className="name">{name}</span>
+//       <span className="quantity"> </span>
+//       <div className="arrow" onClick={removeItemHandler}>
+//         &#10094;
+//       </div>
+
+//       <span className="value">{quantity}</span>
+//       <div className="arrow" onClick={addItemHandler}>
+//         &#10095;
+//       </div>
+
+//       <span className="price">{price}</span>
+//       <div className="remove-button" onClick={clearItemHandler}>
+//         &#10005;
+//       </div>
+//     </CheckoutItemContainer>
+//   );
+// };
+
+// export default CheckoutItem;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,28 +1,67 @@
-import './product-card.styles.scss';
-import { useContext } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addItemToCart } from '../../store/cart/cart.reducer';
+
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-import { CartContext } from '../../contexts/cart.context';
+
+import {
+  ProductCartContainer,
+  Footer,
+  Name,
+  Price,
+} from './product-card.styles';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  const { addItemToCart } = useContext(CartContext);
+  const dispatch = useDispatch();
 
-  const addProductToCart = () => addItemToCart(product);
+  const addProductToCart = () => dispatch(addItemToCart(product));
+
   return (
-    <div className="product-card-container">
+    <ProductCartContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button
         buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to card
       </Button>
-    </div>
+    </ProductCartContainer>
   );
 };
 
 export default ProductCard;
+
+// import './product-card.styles.scss';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectCartItems } from '../../store/cart/cart.selector';
+// import { addItemToCart } from '../../store/cart/cart.reducer';
+// import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+// const ProductCard = ({ product }) => {
+//   const { name, price, imageUrl } = product;
+//   const cartItems = useSelector(selectCartItems);
+//   const dispatch = useDispatch();
+//   const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
+//   return (
+//     <div className="product-card-container">
+//       <img src={imageUrl} alt={`${name}`} />
+//       <div className="footer">
+//         <span className="name">{name}</span>
+//         <span className="price">{price}</span>
+//       </div>
+//       <Button
+//         buttonType={BUTTON_TYPE_CLASSES.inverted}
+//         onClick={addProductToCart}
+//       >
+//         Add to card
+//       </Button>
+//     </div>
+//   );
+// };
+
+// export default ProductCard;
